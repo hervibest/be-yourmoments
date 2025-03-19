e85dab76bf1dae020290a7dee3e17e0a4d79760a
hervi <hervipro@gmail.com>
2025-03-19 14:17:27 +0700

    fix : Refactor minio in upload adapter into config

--- photo-svc/internal/adapter/upload_adapter.go
+++ photo-svc/internal/adapter/upload_adapter.go
@@ -1,90 +1,37 @@
 package adapter
 
 import (
-	"be-yourmoments/photo-svc/internal/helper/logger"
-	"be-yourmoments/photo-svc/internal/helper/utils"
+	"be-yourmoments/photo-svc/internal/config"
 	"be-yourmoments/photo-svc/internal/model"
 
 	"context"
 	"fmt"
-	"log"
 	"mime/multipart"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/minio/minio-go/v7"
-	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
-type Minio interface {
+type UploadAdapter interface {
 	UploadFile(ctx context.Context, file *multipart.FileHeader, path string) (*model.MinioFileResponse, error)
 	DeleteFile(ctx context.Context, fileName string) (bool, error)
 }
 
-type minioImpl struct {
-	MinioClient     *minio.Client
-	minioBucketName string
-	enpoint         string
-	logs            *logger.Log
+type uploadAdapter struct {
+	minio *config.Minio
 }
 
-func NewMinio() Minio {
-	logger := logger.New("minio")
-	ctx := context.Background()
-	var minioClient *minio.Client
-	var err error
-
-	minioHost := utils.GetEnv("MINIO_HOST")
-	minioPort := utils.GetEnv("MINIO_PORT")
-	minioRootUser := utils.GetEnv("MINIO_ROOT_USER")
-	minioRootPassword := utils.GetEnv("MINIO_ROOT_PASSWORD")
-	minioTicketsBucket := utils.GetEnv("MINIO_TICKETS_BUCKET")
-	minioLocation := utils.GetEnv("MINIO_LOCATION")
-	endpoint := minioHost + ":" + minioPort
-
-	minioClient, err = minio.New(endpoint, &minio.Options{
-		Creds:  credentials.NewStaticV4(minioRootUser, minioRootPassword, ""),
-		Secure: false,
-	})
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	err = minioClient.MakeBucket(ctx, minioTicketsBucket, minio.MakeBucketOptions{Region: minioLocation})
-	if err != nil {
-		exists, errBucketExists := minioClient.BucketExists(ctx, minioTicketsBucket)
-		if errBucketExists == nil && exists {
-			log.Printf("We already own %s\n", minioTicketsBucket)
-		} else {
-			log.Fatalln(err)
-		}
-	} else {
-		log.Printf("Successfully created %s\n", minioTicketsBucket)
-	}
-
-	log.Printf("Successfully connected %s\n", minioTicketsBucket)
-
-	return &minioImpl{
-		MinioClient:     minioClient,
-		minioBucketName: minioTicketsBucket,
-		logs:            logger,
+func NewUploadAdpater(minio *config.Minio) UploadAdapter {
+	return &uploadAdapter{
+		minio: minio,
 	}
 }
 
-func (m *minioImpl) getBucketName() string {
-	BucketName := m.minioBucketName
-	return BucketName
-}
-
-func (m *minioImpl) getEndpoint() string {
-	Endpoint := m.enpoint
-	return Endpoint
-}
-
-func (m *minioImpl) UploadFile(ctx context.Context, file *multipart.FileHeader, path string) (*model.MinioFileResponse, error) {
+func (a *uploadAdapter) UploadFile(ctx context.Context, file *multipart.FileHeader, path string) (*model.MinioFileResponse, error) {
 	uploadFile, err := file.Open()
 	if err != nil {
-		m.logs.Error("parse file error" + err.Error())
+		a.minio.Logs.Error("parse file error" + err.Error())
 		return nil, fiber.NewError(fiber.StatusUnprocessableEntity, err.Error())
 	}
 	defer uploadFile.Close()
@@ -92,11 +39,11 @@ func (m *minioImpl) UploadFile(ctx context.Context, file *multipart.FileHeader, 
 	fileKey := path + string(RandomNumber(31)) + "_" + file.Filename
 	contentType := file.Header.Get("Content-Type")
 
-	s3PutObjectOutput, err := m.MinioClient.PutObject(ctx, m.getBucketName(), fileKey, uploadFile, file.Size, minio.PutObjectOptions{
+	s3PutObjectOutput, err := a.minio.MinioClient.PutObject(ctx, a.minio.GetBucketName(), fileKey, uploadFile, file.Size, minio.PutObjectOptions{
 		ContentType: contentType,
 	})
 	if err != nil {
-		m.logs.Error("failed to upload file to S3" + err.Error())
+		a.minio.Logs.Error("failed to upload file to S3" + err.Error())
 
 		return nil, fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
@@ -109,9 +56,9 @@ func (m *minioImpl) UploadFile(ctx context.Context, file *multipart.FileHeader, 
 	fileResponse.ETag = s3PutObjectOutput.ETag
 	fileResponse.Expiration = s3PutObjectOutput.Expiration
 
-	fileURL, err := m.MinioClient.PresignedGetObject(ctx, m.getBucketName(), fileKey, 1*time.Hour, nil)
+	fileURL, err := a.minio.MinioClient.PresignedGetObject(ctx, a.minio.GetBucketName(), fileKey, 1*time.Hour, nil)
 	if err != nil {
-		m.logs.Error("failed to generate presigned URL:" + err.Error())
+		a.minio.Logs.Error("failed to generate presigned URL:" + err.Error())
 
 		return nil, fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
@@ -124,9 +71,9 @@ func (m *minioImpl) UploadFile(ctx context.Context, file *multipart.FileHeader, 
 	return fileResponse, nil
 }
 
-func (m *minioImpl) DeleteFile(ctx context.Context, fileName string) (bool, error) {
+func (a *uploadAdapter) DeleteFile(ctx context.Context, fileName string) (bool, error) {
 
-	err := m.MinioClient.RemoveObject(ctx, m.getBucketName(), fileName, minio.RemoveObjectOptions{ForceDelete: true})
+	err := a.minio.MinioClient.RemoveObject(ctx, a.minio.GetBucketName(), fileName, minio.RemoveObjectOptions{ForceDelete: true})
 	if err != nil {
 		return false, fmt.Errorf("failed to delete file: %w", err)
 	}
